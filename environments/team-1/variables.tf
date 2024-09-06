4cdeb09e535cbc887170134f1920176733792d33
dipanjanr640 <159446245+dipanjanr640@users.noreply.github.com>
2024-09-06 20:54:54 +0530

    Update variables.tf

--- environments/team-1/variables.tf
+++ environments/team-1/variables.tf
@@ -1,1 +1,24 @@
+variable "vm_name" {
+  default = "team1-vm"
+}
+
+variable "location" {
+  default = "East US"
+}
+
+variable "resource_group_name" {
+  default = "team1-rg"
+}
+
+variable "vm_size" {
+  default = "Standard_DS1_v2"
+}
+
+variable "admin_username" {
+  default = "admin"
+}
+
+variable "admin_password" {
+  default = "Indira033@"
+}
 

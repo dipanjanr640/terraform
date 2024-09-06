bda70d983e468be1b0a35346dac006f8cf3cb3c2
Dipanjan <dipanjanr640@gmail.com>
2024-09-06 21:49:08 +0530

    correction update

--- modules/azure-vm/main.tf
+++ modules/azure-vm/main.tf
@@ -61,34 +61,25 @@ resource "azurerm_network_security_group" "nsg" {
 }
 
 # Virtual Machine
-resource "azurerm_virtual_machine" "vm" {
+resource "azurerm_linux_virtual_machine" "vm" {
   name                  = var.vm_name
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.nic.id]
-  vm_size               = var.vm_size
+  size               = var.vm_size
+  admin_username      = var.admin_username
+  admin_password      = var.admin_username
+  disable_password_authentication = false
 
-  storage_os_disk {
-    name              = "${var.vm_name}_os_disk"
+  os_disk {
     caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
+    storage_account_type = "Standard_LRS"
   }
 
-  os_profile {
-    computer_name  = var.vm_name
-    admin_username = var.admin_username
-    admin_password = var.admin_password
-  }
-
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-
-  source_image_reference {
+   source_image_reference {
     publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
     version   = "latest"
   }
 }
--- modules/azure-vm/variables.tf
+++ modules/azure-vm/variables.tf
@@ -1,11 +1,44 @@
-variable "name" {
-  type = string
+# Name of the virtual machine
+variable "vm_name" {
+  description = "The name of the virtual machine."
+  type        = string
 }
+
+# Location where the resources will be created
 variable "location" {
-  type = string
+  description = "The Azure region where the resources will be created."
+  type        = string
 }
-variable "rg_name" {
-  type = string
+
+# Name of the resource group
+variable "resource_group_name" {
+  description = "The name of the resource group."
+  type        = string
 }
-variable "nic" {
+
+# Size of the virtual machine
+variable "vm_size" {
+  description = "The size of the virtual machine."
+  type        = string
+  default     = "Standard_DS1_v2"  # Set a default VM size, can be overridden
 }
+
+# Network interface ID (passed from the configuration calling the module)
+variable "network_interface_id" {
+  description = "The ID of the network interface to attach to the virtual machine."
+  type        = string
+}
+
+# Admin username for the VM
+variable "admin_username" {
+  description = "The admin username for the virtual machine."
+  type        = string
+}
+
+# Admin password for the VM (for simplicity, using string here, but you should use sensitive data management for passwords)
+variable "admin_password" {
+  description = "The admin password for the virtual machine."
+  type        = string
+}
+
+# Additional variables (optional) for more customization can be added here as needed

b4d2d05b4f809e45b485538bc1ac4009a3124e96
dipanjanr640 <159446245+dipanjanr640@users.noreply.github.com>
2024-09-06 20:51:43 +0530

    Update main.tf

--- modules/azure-vm/main.tf
+++ modules/azure-vm/main.tf
@@ -1,51 +1,94 @@
+# Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
 
+# Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.resource_group_name}-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
 
-resource "azurerm_linux_virtual_machine" "terraform_vm1" {
-  name                = var.name
-  resource_group_name = var.rg_name
-  location            = var.location
-  size                = "Standard_F2"
-  admin_username      = "ubuntu"
-  admin_password      = "Indira033@"
-  disable_password_authentication = false
-  network_interface_ids = [
-    var.nic
-  ]
+# Subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "${var.resource_group_name}-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
 
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
+# Network Interface
+resource "azurerm_network_interface" "nic" {
+  name                = "${var.vm_name}-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+# Public IP
+resource "azurerm_public_ip" "pip" {
+  name                = "${var.vm_name}-pip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+}
+
+# Network Security Group
+resource "azurerm_network_security_group" "nsg" {
+  name                = "${var.resource_group_name}-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "allow_ssh"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# Virtual Machine
+resource "azurerm_virtual_machine" "vm" {
+  name                  = var.vm_name
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  vm_size               = var.vm_size
+
+  storage_os_disk {
+    name              = "${var.vm_name}_os_disk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
   }
 
-   source_image_reference {
+  os_profile {
+    computer_name  = var.vm_name
+    admin_username = var.admin_username
+    admin_password = var.admin_password
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+
+  source_image_reference {
     publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
     version   = "latest"
   }
- 
-}
-resource "null_resource" "pre_requisite_install" {
-  provisioner "remote-exec" {
-    inline = [
-      "sudo apt-get update",
-      "sudo apt install -y python3",
-      "sudo apt install -y ansible",
-      "sudo sed -i 's/no/yes/' /etc/ssh/sshd_config.d/60-cloudimg-settings.conf",
-      "sudo systemctl restart ssh",
-      "sudo apt-get install -y sshpass"
-    ]
-
-    connection {
-      type        = "ssh"
-      user        = "ubuntu"
-      password    = "Indira033@"
-      host        = azurerm_linux_virtual_machine.terraform_vm1.public_ip_address
-    }
-  }
-  depends_on = [ azurerm_linux_virtual_machine.terraform_vm1 ]
-  
-}
-output "host_ip" {
-  value = azurerm_linux_virtual_machine.terraform_vm1.public_ip_address
 }

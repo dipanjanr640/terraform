62763a24882ae710dcefe502f17e09cf88e2a62f
dipanjanr640 <159446245+dipanjanr640@users.noreply.github.com>
2024-09-06 20:53:56 +0530

    Update main.tf

--- environments/team-1/main.tf
+++ environments/team-1/main.tf
@@ -1,7 +1,14 @@
-module "manage_myvm" {
-  source = "./module/azure-vm"
-  location = azurerm_resource_group.terraform_rg.location
-  name = "managevm121212"
-  nic = azurerm_network_interface.terraform_network_interface1.id
-  rg_name = azurerm_resource_group.terraform_rg.name
+module "team1_vm" {
+  source = "../../modules/vm"  # Path to the module
+
+  # Pass input variables
+  vm_name              = var.vm_name
+  location             = var.location
+  resource_group_name  = var.resource_group_name
+  network_interface_id = azurerm_network_interface.nic.id
+  vm_size              = var.vm_size
+  admin_username       = var.admin_username
+  admin_password       = var.admin_password
 }
+
+# Optionally define any team-specific resources, like outputs or additional configurations.

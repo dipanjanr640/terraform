798a2fd73631d1aa53a17219eac632bef85af2ae
dipanjanr640 <159446245+dipanjanr640@users.noreply.github.com>
2024-09-06 20:39:56 +0530

    Update main.tf

--- environments/team-1/main.tf
+++ environments/team-1/main.tf
@@ -1,1 +1,7 @@
-
+module "manage_myvm" {
+  source = "./module/azure-vm"
+  location = azurerm_resource_group.terraform_rg.location
+  name = "managevm121212"
+  nic = azurerm_network_interface.terraform_network_interface1.id
+  rg_name = azurerm_resource_group.terraform_rg.name
+}

d8c71d67bad088f869a16c4c134abdb12fefe783
Dipanjan <dipanjanr640@gmail.com>
2024-09-20 22:18:59 +0530

    backend update

--- environments/team-1/backend.tf
+++ environments/team-1/backend.tf
@@ -1,1 +1,9 @@
-
+  terraform {
+    backend "azurerm" {
+    resource_group_name = "ansible_rg_dev"
+    storage_account_name = "tfbackendstorage12345678"
+    container_name = "tfbackends"
+    key = "terraform.tfstate"
+    access_key = "/9/4pyDdoVVLsH/XYGlmdqQq0e5mB0LyB2pVBDIs6fe+qqfMpr+oN6LnYYDOwf0XKXJ9MAXj6fxf+AStas+IOg=="    
+  }
+  }
